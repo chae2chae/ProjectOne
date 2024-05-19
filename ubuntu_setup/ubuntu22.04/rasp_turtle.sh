b35790d43a3e0bfaf4e61c0695ee1c1ae51f2a41
leggiero-crescendo <ktxlee779@gmail.com>
2024-05-19 09:57:04 +0900

    6weeks

--- ubuntu_setup/ubuntu22.04/rasp_turtle.sh
+++ ubuntu_setup/ubuntu22.04/rasp_turtle.sh
@@ -1,32 +1,36 @@
-echo "export ROS_DOMAIN_ID=30" #TURTLEBOT3 >> ~/.bashrc
-echo 'export LDS_MODEL=LDS-01' >> ~/.bashrc
-sudo apt install python3-argcomplete python3-colcon-common-extensions libboost-system-dev build-essential
-sudo apt install ros-humble-hls-lfcd-lds-driver
-sudo apt install ros-humble-turtlebot3-msgs
-sudo apt install ros-humble-dynamixel-sdk
-sudo apt install libudev-dev
-mkdir -p ~/turtlebot3_ws/src && cd ~/turtlebot3_ws/src
-git clone -b humble-devel https://github.com/ROBOTIS-GIT/turtlebot3.git
-git clone -b ros2-devel https://github.com/ROBOTIS-GIT/ld08_driver.git
-cd ~/turtlebot3_ws/src/turtlebot3
-rm -r turtlebot3_cartographer turtlebot3_navigation2
-cd ~/turtlebot3_ws/
-echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc
-source ~/.bashrc
-colcon build --symlink-install --parallel-workers 1
-echo 'source ~/turtlebot3_ws/install/setup.bash' >> ~/.bashrc
-source ~/.bashrc
-# OpenCR
-sudo dpkg --add-architecture armhf
-sudo apt update
-sudo apt install libc6:armhf
+# echo "export ROS_DOMAIN_ID=30" #TURTLEBOT3 >> ~/.bashrc
+# echo 'export LDS_MODEL=LDS-01' >> ~/.bashrc
+# sudo apt install python3-argcomplete python3-colcon-common-extensions libboost-system-dev build-essential
+# # sudo apt install ros-humble-hls-lfcd-lds-driver
+# # sudo apt install ros-humble-turtlebot3-msgs
+# # sudo apt install ros-humble-dynamixel-sdk
+# sudo apt install libudev-dev
+# mkdir -p ~/turtlebot3_ws/src && cd ~/turtlebot3_ws/src
+# git clone -b humble-devel https://github.com/ROBOTIS-GIT/DynamixelSDK.git
+# git clone -b humble-devel https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
+# git clone -b humble-devel https://github.com/ROBOTIS-GIT/turtlebot3.git
+# git clone -b humble-devel https://github.com/ROBOTIS-GIT/hls_lfcd_lds_driver.git
+# git clone -b humble-devel https://github.com/ROBOTIS-GIT/turtlebot3.git
+# git clone -b ros2-devel https://github.com/ROBOTIS-GIT/ld08_driver.git
+# cd ~/turtlebot3_ws/src/turtlebot3
+# rm -r turtlebot3_cartographer turtlebot3_navigation2
+# cd ~/turtlebot3_ws/
+# echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc
+# source ~/.bashrc
+# colcon build --symlink-install --parallel-workers 1
+# echo 'source ~/turtlebot3_ws/install/setup.bash' >> ~/.bashrc
+# source ~/.bashrc
+# # OpenCR
+# sudo dpkg --add-architecture armhf
+# sudo apt update
+# sudo apt install libc6:armhf
 
-echo 'export OPENCR_PORT=/dev/ttyACM0' >> ~/.bashrc
-echo 'export OPENCR_MODEL=burger' >> ~/.bashrc
-rm -rf ./opencr_update.tar.bz2
+# echo 'export OPENCR_PORT=/dev/ttyACM0' >> ~/.bashrc
+# echo 'export OPENCR_MODEL=burger' >> ~/.bashrc
+# rm -rf ./opencr_update.tar.bz2
 
-wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS2/latest/opencr_update.tar.bz2
-tar -xvf ./opencr_update.tar.bz2
+# wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS2/latest/opencr_update.tar.bz2
+# tar -xvf ./opencr_update.tar.bz2
 
-cd ~/opencr_update
-./update.sh $OPENCR_PORT $OPENCR_MODEL.opencr
+# cd ~/opencr_update
+# ./update.sh $OPENCR_PORT $OPENCR_MODEL.opencr
